14cadbc0ee1772990404734aefa0b47dbe87fc0b
Yakub Moshood <moshoodolabanji22@gmail.com>
2022-12-28 18:21:59 +0100

    second commit

--- README.md
+++ README.md
@@ -27,8 +27,8 @@ Users should be able to:
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://your-solution-url.com)
-- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+- Solution URL: [Add solution URL here](https://github.com/prime1999/Rating-Component-App)
+- Live Site URL: [Add live site URL here](https://zesty-bavarois-ff65f2.netlify.app)
 
 ## My process
 
--- main.js
+++ main.js
@@ -2,21 +2,22 @@
 const content = document.querySelector(".content");
 const btn = document.querySelector(".btn");
 const rate = document.querySelectorAll(".rate");
-const rating = document.querySelector(".rating");
+const myForm = document.querySelector(".myForm");
+const ifChecked = document.querySelectorAll("input");
+const li = document.querySelectorAll("li");
 let selected;
 
-rating.addEventListener("click", (e) => {
-  if (e.target.classList.contains("rate")) {
-    removeCheck();
-    e.target.classList.add("active");
-    selected = e.target.innerText;
-  }
-});
+myForm.addEventListener("click", getFeedBack);
+// if (e.target.classList.contains("rate")) {
+//   removeCheck();
+//   e.target.classList.add("active");
+//   selected = e.target.innerText;
+// }
 
 btn.addEventListener("click", (e) => {
   content.innerHTML = `
     <div class="img1">
-    <img src="images/illustration-thank-you.svg" alt="" />
+    <img src="images/illustration-thank-you.svg" alt="" aria-hidden=“true” />
   </div>
   <div class="selected">
     <p>you selected ${selected} out of 5</p>
@@ -31,6 +32,18 @@ btn.addEventListener("click", (e) => {
     `;
 });
 
+//Function to remove class
 function removeCheck() {
   rate.forEach((rate) => rate.classList.remove("active"));
+}
+//Function to addClass
+function getFeedBack(e) {
+  if (e.target.checked) selected = e.target.value;
+
+  li.forEach((list) => {
+    if (list.lastElementChild.checked) {
+      removeCheck();
+      list.firstElementChild.classList.add("active");
+    }
+  });
 }
